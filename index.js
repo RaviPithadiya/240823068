78658df19d63fd65df4c12ac64b46a29eead5fb6
Your Name <ravipithadiya316@gmail.com>
2025-07-14 11:23:09 +0530

    1 program

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log(name)
 
 const surname = "pithadiya"
 console.log(surname)
+
+//1 even or odd
+
+let number = 7;
+if(number % 2 == 0){
+    console.log("Even");
+}else{
+    console.log("odd");
+}
