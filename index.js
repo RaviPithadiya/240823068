1ae78ceec25b95a6cead1541248cc69d93d017e7
Your Name <ravipithadiya316@gmail.com>
2025-07-14 11:31:55 +0530

    2  program

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ console.log(surname)
 
 let number = 7;
 if(number % 2 == 0){
-    console.log("Even");
+    console.log("Even")
 }else{
-    console.log("odd");
+    console.log("odd")
 }
+
+//2 find maximum of two number
+
+let number1 = 5;
+let number2 = 9;
+if(number1>number2){
+    console.log("number 1 is greater")
+}else{
+    console.log("number 2 is greater")
+
+}
+
+
+
